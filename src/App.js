25b5a95bf9c89605c4e718f047b661e5c5971eae
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-16 16:08:29 -0800

    auth page works

--- src/App.js
+++ src/App.js
@@ -12,15 +12,27 @@ import ListPage from './ListPage';
 
 import './App.css';
 
+
 export default function App() {
   // track the user in state
+  const [user, setUser] = useState(localStorage.getItem('supabase.auth.token'));
 
   // add a useEffect to get the user and inject the user object into state on load
+  useEffect(() => {
+    const user = getUser;
+
+    setUser(user);
+
+  }, []);
 
   async function handleLogout() {
     // call the logout function
+    logout();
+
     // clear the user in state
+    setUser('');
   }
+
 
   return (
     <Router>
@@ -32,6 +44,10 @@ export default function App() {
           <Switch>
             <Route exact path="/">
               {/* if there is a user, redirect to the list. Otherwise, render the auth page. Note that the AuthPage will need a function called setUser that can set the user state in App.js */}
+              { user
+                ? <ListPage />
+                : <AuthPage setUser={setUser} />
+              }
             </Route>
             <Route exact path="/shopping-list">
               {/* if there's a user, take them to the list page. Otherwise, redirect them to the home/auth page */}
